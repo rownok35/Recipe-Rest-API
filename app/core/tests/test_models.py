8ab6b1439e8dd11e9a45e692520b1fe033501b6d
rownok35 <rownokrezwan@gmail.com>
2021-09-10 13:56:30 +0600

    added ingredient feature

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,4 +1,4 @@
-
+from unittest.mock import patch
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
--- app/recipe/urls.py
+++ app/recipe/urls.py
@@ -4,13 +4,14 @@ from rest_framework.routers import DefaultRouter
 
 from recipe import views
 
+app_name = 'recipe'
 
 router = DefaultRouter()
 router.register('tags', views.TagViewSet)
 router.register('ingredients', views.IngredientViewSet)
 router.register('recipes', views.RecipeViewSet)
 
-app_name = 'recipe'
+
 
 urlpatterns = [
     path('', include(router.urls))
